bc71e3a32ac05d653fe07771618c92dad91604db
anurudhp <anurudhp97@gmail.com>
2021-09-01 11:20:53 +0530

    update headers

--- include/Standalone/StandaloneOps.h
+++ include/Standalone/StandaloneOps.h
@@ -9,9 +9,15 @@
 #ifndef STANDALONE_STANDALONEOPS_H
 #define STANDALONE_STANDALONEOPS_H
 
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
+#include "mlir/IR/FunctionSupport.h"
 #include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #define GET_OP_CLASSES
--- lib/Standalone/StandaloneOps.cpp
+++ lib/Standalone/StandaloneOps.cpp
@@ -8,7 +8,10 @@
 
 #include "Standalone/StandaloneOps.h"
 #include "Standalone/StandaloneDialect.h"
+#include "mlir/IR/Builders.h"
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/PatternMatch.h"
+#include "llvm/Support/FormatVariadic.h"
 
 #define GET_OP_CLASSES
 #include "Standalone/StandaloneOps.cpp.inc"
